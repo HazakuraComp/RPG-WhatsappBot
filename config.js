393abff852472d4df6fdda0a95d3881490bd73a2
Ren Aldridge <daviddarren220@gmail.com>
2025-06-16 07:37:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 /*
-Base bot Case × Plugins by ArdSvt
+Base RPG WhatsApp Bot by Renata
 ~ Created on 5/31/2025
-~ Contact me wa.me/6283861772386
-~ Database MongoDB ( untuk uri nya bisa saya bantu buatkan, chat saja melalui nomor whatsapp diatas )
-~ Free fitur jadibot QR/Pairing Code
-~ Bisa Custom Pairing Code
+~ Contact me wa.me/6287756593163
+~ MongoDB Database (I can help you create the uri, just chat via the whatsapp number above)
+~ Free f
 */
 
-global.creator = [ "6283861772386"]
-global.mongodb_uri = "URI NYA BISA SAYA BANTU BUATKAN CHAT SAJA KE NOMER WHATSAPP DIATAS"
+global.creator = ["6287756593163"]
+global.mongodb_uri = "CAN I HELP YOU CREATE A URI, JUST CHAT TO THE WHATSAPP NUMBER ABOVE"
 global.pairing_code = "ARDACRTR"
 
 let file = require.resolve(__filename)
