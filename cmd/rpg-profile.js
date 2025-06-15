c6f604133966bf1806b29311475c443defee0763
Ren Aldridge <daviddarren220@gmail.com>
2025-06-16 07:35:48 +0800

    Create rpg-profile.js

---
+++ cmd/rpg-profile.js
@@ -0,0 +1,22 @@
+module.exports = {
+  command: ['profile'],
+  tags: ['rpg'],
+  help: ['profile'],
+  func: async (m, { mongo }) => {
+    const user = await mongo.db('rpg').collection('users').findOne({ uid: m.sender }) || {};
+    const coin = user.coin || 0;
+    const exp = user.exp || 0;
+    const hp = user.hp ?? 100;
+    const inv = user.inventory || {};
+
+    m.reply(`👤 *Profil RPG*
+HP: ${hp}
+EXP: ${exp}
+Coin: ${coin}
+
+📦 Inventory:
+• Iron: ${inv.iron || 0}
+• Gold: ${inv.gold || 0}
+• Diamond: ${inv.diamond || 0}`);
+  }
+};
