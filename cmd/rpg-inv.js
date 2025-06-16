207f4c182bee383cd99806f880f9de155b4f6566
Ren Aldridge <daviddarren220@gmail.com>
2025-06-16 08:23:07 +0800

    Update rpg-inv.js

--- cmd/rpg-inv.js
+++ cmd/rpg-inv.js
@@ -3,7 +3,7 @@ module.exports = {
   tags: ['rpg'],
   help: ['inventory'],
   func: async (m, { mongo }) => {
-    const user = await mongo.db('rpg').collection('users').findOne({ uid: m.sender }) || {};
+    const user = await global.database.collection('users').findOne({ uid: m.sender }) || {};
     const inv = user.inventory || {};
 
     m.reply(`📦 Inventory kamu:\n🪨 Iron: ${inv.iron || 0}\n💰 Gold: ${inv.gold || 0}\n💎 Diamond: ${inv.diamond || 0}`);
