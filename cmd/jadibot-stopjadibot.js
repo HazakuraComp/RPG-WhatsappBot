fb1b2aa481156a9768e594a1a61b32fbbf7ab99b
Ren Aldridge <daviddarren220@gmail.com>
2025-06-26 11:24:32 +0800

    Update jadibot-stopjadibot.js

--- cmd/jadibot-stopjadibot.js
+++ cmd/jadibot-stopjadibot.js
@@ -5,7 +5,7 @@ command: [ "stopjadibot" ],
 tags: [ "Jadibot", "Main" ],
 private: true,
 func: async (m, { ard, text, isSct }) => {
-if (!Collection.conns.has(m.sender.split("@")[0]) return m.reply("Kamu tidak sedang jadibot!")
+if (!Collection.conns.has(m.sender.split("@")[0])) return m.reply("Kamu tidak sedang jadibot!")
 const number = m.sender.split('@')[0];
     await ard.sendText(m.chat, "Success stop jadibot!", m);
     await database.collection("jadibot").deleteOne({id:m.sender})
